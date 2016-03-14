bc91a29e379541f0036e95b2f991e56e94689d07
Jason Fieldman <jason@fieldman.org>
2016-03-14 09:03:29 -0700

    trying to add new.. but issues

--- Examples/CadmiumBasicExample/CadmiumBasicExample/ViewController.swift
+++ Examples/CadmiumBasicExample/CadmiumBasicExample/ViewController.swift
@@ -18,11 +18,20 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.whiteColor()
         self.title = "Example"
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .Plain, target: self, action: "handleAdd:")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func handleAdd(button: UIButton) {
+        Cd.transact {
+            let newItem = ExampleItem.create()
+            newItem.name = "New"
+        }
     }
     
 }
