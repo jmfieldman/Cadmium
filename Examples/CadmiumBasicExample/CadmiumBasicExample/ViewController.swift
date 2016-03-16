3e72b23c52e8fbbdb68660eca4fa7dab45a16453
Jason Fieldman <jason@fieldman.org>
2016-03-16 07:49:35 -0700

    cleaning up a bit

--- Examples/CadmiumBasicExample/CadmiumBasicExample/ViewController.swift
+++ Examples/CadmiumBasicExample/CadmiumBasicExample/ViewController.swift
@@ -17,26 +17,27 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.whiteColor()
-        self.title = "Example"
+        self.title = "Cadmium Example"
         
         self.navigationItem.leftBarButtonItem  = UIBarButtonItem(title: "Add",  style: .Plain, target: self, action: "handleAdd:")
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .Plain, target: self, action: "handleEdit:")
-        
+    
+        /* Create a fetched results controller that will list all of our example items  (sorted by name) */
         fetchedResultsController = CdFetchedResultsController(
             fetchRequest: Cd.objects(ExampleItem.self).sorted("name").nsFetchRequest,
             sectionNameKeyPath: nil,
             cacheName: nil)
         
+        /* Using the delegate automation allows Cadmium to automatically update the UITableView
+           data source when changes occur to the underlying model. */
         fetchedResultsController?.automateDelegation(forTable: self.tableView)
+        
+        /* Call performFetch to initiate the controller */
         try! fetchedResultsController?.performFetch()
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    /* Adds an ExampleItem with a random 10-character name */
     func handleAdd(button: UIBarButtonItem) {
         let allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         let allowedCharsCount = UInt32(allowedChars.characters.count)
@@ -48,6 +49,14 @@ class ViewController: UITableViewController {
             randomString += String(newCharacter)
         }
         
+        /* managed objects must be inserted in a transaction (not the main thread).
+           An implicit commit finalizes the transaction after it is finished.  
+        
+           You can call Cd.cancelImplicitCommit() to disable this implicit commit,
+           which is useful if you are canceling any changes made during the transaction.
+        
+           You can call Cd.commit() anytime to explicitly save any changes in the transaction
+           up to the call. */
         Cd.transact {
             let newItem = Cd.create(ExampleItem.self)
             newItem.name = randomString
@@ -71,25 +80,36 @@ class ViewController: UITableViewController {
 
 extension ViewController {
     
+    /* We don't have sections with table automation, so we can assume the object count is the number of rows. */
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fetchedResultsController!.fetchedObjects!.count
+        return fetchedResultsController!.fetchedObjects?.count ?? 0
     }
     
+    /* Extract the item from the fetched results controller and display a cell with its information */
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCellWithIdentifier("cell") ?? UITableViewCell(style: .Subtitle, reuseIdentifier: "cell")
-        if let item = fetchedResultsController?.objectAtIndexPath(indexPath) as? ExampleItem {
-            cell.textLabel?.text = item.name
-            cell.detailTextLabel?.text = "number of taps: \(item.numberTaps)"
+
+        guard let item = fetchedResultsController?.objectAtIndexPath(indexPath) as? ExampleItem else {
+            return cell
         }
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "number of taps: \(item.numberTaps)"
         return cell
     }
     
+    /* Acquire the item from the fetched results controller at the proper index, and increment its tap count */
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         guard let item = fetchedResultsController?.objectAtIndexPath(indexPath) as? ExampleItem else {
             return
         }
         
+        /* The objects from the fetch controller are from the main thread's read-only context.
+           They need to be passed into a write transaction to be modified.
+           An implicit commit occurs when the transaction is complete and changes are
+           written to disk.  The main thread context is notified of this change and
+           the fetch controller will updated automatically in the main thread. */
         Cd.transact {
             guard let txItem = Cd.useInCurrentContext(item) else {
                 return
@@ -110,11 +130,13 @@ extension ViewController {
         return .Delete
     }
     
+    /* Attempt to delete the item at the specified index */
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         guard let item = fetchedResultsController?.objectAtIndexPath(indexPath) as? ExampleItem else {
             return
         }
         
+        /* Objects can only be deleted in transactions, similar to object modification. */
         Cd.transact {
             guard let txItem = Cd.useInCurrentContext(item) else {
                 return
