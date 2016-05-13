b9b5da0c7906110663f8197d92265cd69307f67f
Jason Fieldman <jason@fieldman.org>
2016-05-13 09:26:04 -0700

    initial example addition

---
+++ Examples/CadmiumPromiseExample/CadmiumPromiseExample/AppDelegate.swift
@@ -0,0 +1,63 @@
+//
+//  AppDelegate.swift
+//  CadmiumPromiseExample
+//
+//  Created by Jason Fieldman on 5/13/16.
+//  Copyright © 2016 Jason Fieldman. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import Cadmium
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+
+	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+		
+		try! Cd.initWithSQLStore(momdInbundleID: nil, momdName: "CadmiumBasicExample.momd", sqliteFilename: "example.sqlite")
+		
+		self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+		self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
+		self.window?.makeKeyAndVisible()
+		
+		// Override point for customization after application launch.
+		return true
+	}
+
+	func applicationWillResignActive(application: UIApplication) {
+		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+	}
+
+	func applicationDidEnterBackground(application: UIApplication) {
+		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+	}
+
+	func applicationWillEnterForeground(application: UIApplication) {
+		// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+	}
+
+	func applicationDidBecomeActive(application: UIApplication) {
+		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+	}
+
+	func applicationWillTerminate(application: UIApplication) {
+		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+		// Saves changes in the application's managed object context before the application terminates.
+	}
+
+	// MARK: - Core Data stack
+
+	lazy var applicationDocumentsDirectory: NSURL = {
+	    // The directory the application uses to store the Core Data store file. This code uses a directory named "org.fieldman.CadmiumPromiseExample" in the application's documents Application Support directory.
+	    let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+	    return urls[urls.count-1]
+	}()
+
+}
+
