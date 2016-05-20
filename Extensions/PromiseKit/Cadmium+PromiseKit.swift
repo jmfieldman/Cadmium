df0815d8f92dcd25fd26d188c174340333e43b30
Jason Fieldman <jason@fieldman.org>
2016-05-20 07:55:20 -0700

    removed tabs

--- Extensions/PromiseKit/Cadmium+PromiseKit.swift
+++ Extensions/PromiseKit/Cadmium+PromiseKit.swift
@@ -15,10 +15,10 @@ public enum CdPromiseError : ErrorType {
 }
 
 public extension Cd {
-	
+    
     /** Allows you to perform a standard Cadmium transaction as a Promise */
     public class func transact<U>(serial serial: Bool? = nil, on serialQueue: dispatch_queue_t? = nil, operation: (Void) throws -> U) -> Promise<U> {
-		return Promise { fulfill, reject in
+        return Promise { fulfill, reject in
             dispatch_async(dispatch_get_global_queue(0, 0)) {
                 var result: U!
                 var error: ErrorType?
@@ -35,13 +35,13 @@ public extension Cd {
                     fulfill(result)
                 }
             }
-		}
-	}
-	
+        }
+    }
+    
     /** Allows you to perform a standard Cadmium transaction as a Promise.
         Converts the object to an instance in the operation's context. */
-	public class func transactWith<T: CdManagedObject, U>(object: T, serial: Bool? = nil, on serialQueue: dispatch_queue_t? = nil, operation: (T) throws -> U) -> Promise<U> {
-		return Promise { fulfill, reject in
+    public class func transactWith<T: CdManagedObject, U>(object: T, serial: Bool? = nil, on serialQueue: dispatch_queue_t? = nil, operation: (T) throws -> U) -> Promise<U> {
+        return Promise { fulfill, reject in
             dispatch_async(dispatch_get_global_queue(0, 0)) {
                 var result: U!
                 var error: ErrorType?
@@ -63,13 +63,13 @@ public extension Cd {
                     fulfill(result)
                 }
             }            
-		}
-	}
-	
+        }
+    }
+    
     /** Allows you to perform a standard Cadmium transaction as a Promise.
         Converts the object array to the operation's context. */
-	public class func transactWith<T: CdManagedObject, U>(objects: [T], serial: Bool? = nil, on serialQueue: dispatch_queue_t? = nil, operation: ([T]) throws -> U) -> Promise<U> {
-		return Promise { fulfill, reject in
+    public class func transactWith<T: CdManagedObject, U>(objects: [T], serial: Bool? = nil, on serialQueue: dispatch_queue_t? = nil, operation: ([T]) throws -> U) -> Promise<U> {
+        return Promise { fulfill, reject in
             dispatch_async(dispatch_get_global_queue(0, 0)) {
                 var result: U!
                 var error: ErrorType?
@@ -86,54 +86,54 @@ public extension Cd {
                     fulfill(result)
                 }
             }
-		}
-	}
-	
+        }
+    }
+    
 }
 
 
 public extension Promise {
-	
+    
     /** Allows you to chain a Cadmium transaction into the promise chain with the immediate 'then' style. 
         The argument to the promise operation is treated as a normal non-managed-object instance. */
-	public func thenTransact<U>(serial serial: Bool? = nil, on serialQueue: dispatch_queue_t? = nil, body: (T) throws -> U) -> Promise<U> {
-		
-		return self.thenInBackground { (value: T) -> U in
-			var result: U!
+    public func thenTransact<U>(serial serial: Bool? = nil, on serialQueue: dispatch_queue_t? = nil, body: (T) throws -> U) -> Promise<U> {
+        
+        return self.thenInBackground { (value: T) -> U in
+            var result: U!
             var error: ErrorType?
-			Cd.transactAndWait(serial: serial, on: serialQueue) {
+            Cd.transactAndWait(serial: serial, on: serialQueue) {
                 do {
                     result = try body(value)
                 } catch let _error {
                     error = _error
                 }
-			}
+            }
             if let error = error { throw error }
-			return result
-		}
-		
-	}
-	
+            return result
+        }
+        
+    }
+    
 }
 
 
 
 public extension Promise where T: CdManagedObject {
-	
+    
     /** Allows you to chain a Cadmium transaction into the promise chain with the immediate 'then' style.
         Using this operation when the generic is a CdManagedObject converts the token object to a new
         instance that belongs to the body's context. */
-	public func thenTransactWith<U>(serial serial: Bool? = nil, on serialQueue: dispatch_queue_t? = nil, body: (T) throws -> U) -> Promise<U> {
-		
-		return self.thenInBackground { (value: T?) -> U in
-			guard let _value = value else {
-				throw CdPromiseError.NotAvailableInCurrentContext(value)
-			}
-			
-			var result: U!
+    public func thenTransactWith<U>(serial serial: Bool? = nil, on serialQueue: dispatch_queue_t? = nil, body: (T) throws -> U) -> Promise<U> {
+        
+        return self.thenInBackground { (value: T?) -> U in
+            guard let _value = value else {
+                throw CdPromiseError.NotAvailableInCurrentContext(value)
+            }
+            
+            var result: U!
             var error: ErrorType?
             Cd.transactAndWait(serial: serial, on: serialQueue) {
-				guard let currentObject = Cd.useInCurrentContext(_value) else {
+                guard let currentObject = Cd.useInCurrentContext(_value) else {
                     error = CdPromiseError.NotAvailableInCurrentContext(_value)
                     return
                 }
@@ -145,21 +145,21 @@ public extension Promise where T: CdManagedObject {
                 }
             }
             if let error = error { throw error }
-			return result
-		}
-		
-	}
+            return result
+        }
+        
+    }
     
     /** Use this to send a managed object from a promise chain into the main thread context. */
-	public func thenOnMainWith<U>(body: (T) throws -> U) -> Promise<U> {
-		
-		return self.then { (value: T) -> U in
+    public func thenOnMainWith<U>(body: (T) throws -> U) -> Promise<U> {
+        
+        return self.then { (value: T) -> U in
             guard let currentObject = Cd.useInCurrentContext(value) else {
                 throw CdPromiseError.NotAvailableInCurrentContext(value)
             }
             
-			return try body(currentObject)
-		}
-		
-	}
+            return try body(currentObject)
+        }
+        
+    }
 }
