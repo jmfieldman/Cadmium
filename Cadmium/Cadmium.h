cec89a6daa51192c00ca0f8735238d8d4ecce511
Jason Fieldman <jason@fieldman.org>
2016-09-08 09:25:27 -0700

    commit to trigger travis

--- Cadmium/Cadmium.h
+++ Cadmium/Cadmium.h
@@ -33,3 +33,4 @@ FOUNDATION_EXPORT const unsigned char CadmiumVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <Cadmium/PublicHeader.h>
 
 
+
